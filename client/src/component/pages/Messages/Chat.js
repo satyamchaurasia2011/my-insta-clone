41a762a8dc0a42c491f08f5cf547fbc0ab2b2fc3
Satyam Chaurasia <satyamchaurasia2011@gmail.com>
2021-11-27 01:57:32 +0530

    update

--- client/src/component/pages/Messages/Chat.js
+++ client/src/component/pages/Messages/Chat.js
@@ -44,7 +44,7 @@ function Chat() {
   const handleClose = () => setOpen(false);
   useEffect(() => {
     if (window.screen.width < 600) setRightShow(false);
-    socket.current = io("https://insta-back.herokuapp.com");
+    socket.current = io("ws://insta-back.herokuapp.com/socket.io/?EIO=4&transport=websocket");
     socket.current.on("getMessage", (data) => {
       console.log(data);
       setArrivalMessage({
